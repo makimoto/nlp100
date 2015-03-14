0181ba595f9f5f8895135d0ab3811fbafcd41dc5
Shimpei Makimoto <makimoto@tsuyabu.in>
2015-03-14 15:42:38 +0900

    Add 004

---
+++ 004/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	input := "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+	re, _ := regexp.Compile("[^\\w ]")
+	input = re.ReplaceAllString(input, "")
+	inputSplitted := strings.Split(input, " ")
+
+	oneCharIndex := make(map[int]bool)
+	for _, v := range []int{1, 5, 6, 7, 8, 9, 15, 16, 19} {
+		oneCharIndex[v-1] = true
+	}
+
+	output := make(map[string]string, 20)
+
+	for i, v := range inputSplitted {
+		if oneCharIndex[i] {
+			output[v[0:1]] = v
+		} else {
+			output[v[0:2]] = v
+		}
+	}
+
+	fmt.Println(output)
+}
