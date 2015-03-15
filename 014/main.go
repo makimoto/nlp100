010f96015d3a5c8f738d526073cfa8d30e6b7322
Shimpei Makimoto <makimoto@tsuyabu.in>
2015-03-15 13:38:21 +0900

    Add 014

---
+++ 014/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/k0kubun/pp"
+)
+
+func main() {
+
+	args := os.Args[1:]
+	pp.Println(args)
+	if len(args) == 0 {
+		help()
+	} else if len(args) == 1 {
+		n, err := strconv.Atoi(os.Args[1])
+		errCheck(err)
+		head(os.Stdin, n)
+	} else {
+		n, err := strconv.Atoi(os.Args[1])
+		errCheck(err)
+		fp, err := os.Open(os.Args[2])
+		errCheck(err)
+		defer fp.Close()
+		head(fp, n)
+	}
+}
+
+func head(fp *os.File, n int) {
+	scanner := bufio.NewScanner(fp)
+	c := 0
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		c++
+		if c >= n {
+			break
+		}
+	}
+}
+
+func help() {
+	fmt.Fprintf(os.Stderr, "usage: %s numOfHeadLines [file]\n", os.Args[0])
+}
+
+func errCheck(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+		help()
+		os.Exit(1)
+	}
+}
